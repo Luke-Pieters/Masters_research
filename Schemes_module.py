56ecff233ed3d1374111f6ca11b97702cc36745c
Luke-Pieters <u18015222@tuks.co.za>
2023-02-06 14:28:17 +0200

    Added More Charts

--- Schemes_module.py
+++ Schemes_module.py
@@ -7,8 +7,9 @@ class SPM_uni_chart:
     ''' 
     Statistical Process Monitoring Univariate scheme 
     
-    mean_0 : 
-    sig2_0 : 
+    mean_0  : 
+    sig2_0  : Varinace 
+    L       : 
     
     Optional:
     
@@ -17,10 +18,11 @@ class SPM_uni_chart:
     k   : 
     
     '''
-    def __init__(self,mean_0,sig2_0,**paramaters) -> None:
+    def __init__(self,mean_0,sig2_0,L,**paramaters) -> None:
         self.mean = mean_0
         self.sig2 = sig2_0
-        
+        self.L = L
+        self.chart_history = np.array([mean_0])
         #phi and phi1 
         if "phi" in paramaters: 
             self.phi = paramaters["phi"]
@@ -34,15 +36,29 @@ class SPM_uni_chart:
             self.k = paramaters["k"]    
 
     def check_ooc(self,stat: float,t) -> bool:
-        pass
-    
-    def ub_lb(self) -> np.array:
-        pass
+        '''Given the value of t and the charting statistic at t, 
+        return TRUE if process is OOC'''
+        lb,ub = self.lb_ub(t=t)
+        if (stat <= lb)|(stat>=ub):
+            return True
+        else:
+            return False
+
+    def lb_ub(self,t) -> np.array:
+        '''Returns the lower- and upperbound of the control limits'''
+        v = self.chart_var(t)
+        lb = self.mean - self.L*np.sqrt(v)
+        ub = self.mean + self.L*np.sqrt(v)
+
+        return [lb,ub]
+
 
 #HWMA SCHEME   
 class HWMA(SPM_uni_chart):
     def chart_stat(self,series) -> float:
-        return self.phi*series[-1] + (1-self.phi)*np.mean(series[:-1])
+        St = self.phi*series[-1] + (1-self.phi)*np.mean(series[:-1])
+        self.chart_history = np.append(self.chart_history,St)
+        return St
     
     def chart_var(self,t) -> float:
         if t==1:
@@ -53,34 +69,64 @@ class HWMA(SPM_uni_chart):
 #Extended HWMA SCHEME    
 class EHWMA(SPM_uni_chart):
     def chart_stat(self,series) -> float:
-        pass
+        St = self.phi*series[-1] - self.phi2*series[-2] + (1-self.phi+self.phi2)*np.mean(series[:-1])
+        self.chart_history = np.append(self.chart_history,St)
+        return St
 
     def chart_var(self,t) -> float:   
-        pass
+        if t ==1:
+            return self.sig2*(self.phi**2)
+        else:
+            c1 = self.phi**2
+            c2 = 1-self.phi- (t-2)*self.phi2
+            c3 = t-1
+            c4 = 1-self.phi +self.phi2
+            c5=c3
+            c6=t-2
+            return (self.sig2)*(c1 + ((c2/c3)**2) + ((c4/c5)**2)*(c6))
+
     
 #EWMA SCHEME    
 class EWMA(SPM_uni_chart):
     def chart_stat(self,series) -> float:
-        pass
+        St = self.phi*series[-1] + (1-self.phi)*self.chart_history[-1]
+        self.chart_history = np.append(self.chart_history,St)
+        return St
 
     def chart_var(self,t) -> float:   
-        pass 
+        c1 = self.phi/(2-self.phi)
+        c2 = 1 - (1-self.phi)**(2*t)
+        return (self.sig2)*c1*c2
   
 #Extended EWMA SCHEME    
 class EEWMA(SPM_uni_chart):
     def chart_stat(self,series) -> float:
-        pass
+        St = self.phi*series[-1] - self.phi2*series[-2] + (1- self.phi + self.phi2)*self.chart_history[-1]
+        self.chart_history = np.append(self.chart_history,St)
+        return St
 
     def chart_var(self,t) -> float:   
-        pass  
+        c1 = (self.phi**2)+(self.phi2**2)
+        c2 = 1 - (1- self.phi +self.phi2)**(2*t)
+        c3 = 2*(self.phi-self.phi2) - ((self.phi-self.phi2)**2)
+        c4 = 2*self.phi*self.phi2*(1-self.phi + self.phi2)
+        c5 = 1- ((1-self.phi+self.phi2)**(2*t -2))
+        c6 = c3
+        return self.sig2*(c1*(c2/c3) - c4*(c5/c6))
     
 #Modified EWMA SCHEME    
 class MEWMA(SPM_uni_chart):
     def chart_stat(self,series) -> float:
-        pass
+        St = self.phi*series[-1] + (1-self.phi)*self.chart_history[-1] +self.k*(series[-1]-series[-2])
+        self.chart_history = np.append(self.chart_history,St)
+        return St
 
     def chart_var(self,t) -> float:   
-        pass
+        c1 = self.phi + 2*self.phi*self.k + 2*(self.k**2)
+        c2 = self.phi*((1-self.phi-self.k)**2)*((1-self.phi)**(2*t-2))
+        c3 = 2 - self.phi
+
+        return self.sig2*((c1-c2)/c3)
     
     
     
--- spm_sim_module.py
+++ spm_sim_module.py
@@ -6,16 +6,19 @@ import Schemes_module as spm
 def spm_simulate(chart_obj,distribution,tau=1,mu_0=0,sig_0=1,max_it=1000):
     '''simulate for a chart with set parameters untill OOC, returns the first OOC t'''
     Xt= np.array([])
-    Xt.append(mu_0) #X_0
+    Xt = np.append(Xt,mu_0) #X_0
     ooc = False
     t=0
-    while (ooc==False) | (t<max_it):
+    dist_mean = distribution.mean()
+    dist_std = distribution.std()
+    while (ooc==False) & (t<max_it):
         t += 1
         if t < tau:
-            Xt.append(sts.norm(loc=mu_0,scale=sig_0).rvs(1))
+            Xt = np.append(Xt, sts.norm(loc=mu_0,scale=sig_0).rvs(1))
             St = chart_obj.chart_stat(Xt)
         else:
-            Xt.append(distribution.rvs(1))
+            #sample from given distribution, and standardise
+            Xt= np.append(Xt, (distribution.rvs(1)-dist_mean)/dist_std)
             St = chart_obj.chart_stat(Xt)
             ooc = chart_obj.check_ooc(St,t=t)
             
@@ -26,7 +29,7 @@ def spm_iterate(n,chart_obj,distribution):
     '''iterate simulate function n times for set chart, return ARL,SDRL and MRL'''
     pass
 
-def spm_optimize_h(chart_obj,n,initial_h,target_ARL=200,tol=0.01,max_its=100):
+def spm_optimize_h(chart_obj,n,initial_h,target_ARL=200,tol=0.01,max_its=1000):
     '''run iteratations for set chart with set parameters and use 
     linear interpolation to itteratively find best h to achieve |ARL-target_ARL|<tol'''
     
@@ -47,3 +50,8 @@ def spm_optimize_h(chart_obj,n,initial_h,target_ARL=200,tol=0.01,max_its=100):
 #dist.mean()
 #dist.std()
 
+test_chart = spm.HWMA(0,1,L=2.6,phi=0.1)
+dist = sts.norm(loc=10,scale=1)
+
+first_t = spm_simulate(chart_obj=test_chart,distribution=dist,tau=1,max_it=1000)
+print(first_t)
