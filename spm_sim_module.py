74fa96aa1138d821cfd249fdfab54f390bd10d8c
Luke-Pieters <u18015222@tuks.co.za>
2023-02-11 22:16:41 +0200

    edited optimise func

--- spm_sim_module.py
+++ spm_sim_module.py
@@ -29,7 +29,7 @@ def spm_simulate(chart_obj,distribution,tau=1,mu_0=0,sig_0=1,max_it=1000):
     return t - tau +1 
 
 
-def spm_iterate(n,chart_obj,distribution,tau=1,rep=1,L=None):
+def spm_iterate(n,chart_obj,distribution,tau=1,L=None):
     '''iterate simulate function n times for set chart, return ARL,SDRL and MRL'''
     start_time = time()
     t_arr = np.array([])
@@ -37,16 +37,16 @@ def spm_iterate(n,chart_obj,distribution,tau=1,rep=1,L=None):
     if (L!=None):
         chart_obj.change_L(new_L=L)
     
-    printProgressBar(iteration=0,total=n,prefix='Run {}'.format(rep))
+    printProgressBar(iteration=0,total=n,prefix='Iterate Progress')
     
     for i in range(n):
-        printProgressBar(iteration=i,total=n,prefix='Run {}'.format(rep))
+        printProgressBar(iteration=i,total=n,prefix='Iterate Progress')
         t_arr = np.append(t_arr,spm_simulate(chart_obj=chart_obj,distribution=distribution,tau=tau))
         
     ARL = np.mean(t_arr)
     SDRL = np.std(t_arr)
     MRL = np.median(t_arr)
-    printProgressBar(iteration=n,total=n,prefix='Run {}'.format(rep))
+    printProgressBar(iteration=n,total=n,prefix='Iterate Progress')
  
     run_time =np.round(time()-start_time+0.4,decimals=0)
     print("Time: {} ".format(timedelta(seconds=run_time)))
@@ -55,19 +55,24 @@ def spm_iterate(n,chart_obj,distribution,tau=1,rep=1,L=None):
         
     
 
-def spm_optimize_h(chart_obj,n,initial_h,target_ARL=200,tol=0.01,max_its=1000):
+def spm_optimize_h(chart_obj,n,initial_h,target_ARL=200,tol=0.01,max_its=1000,bounds=(0,3)):
     '''run iteratations for set chart with set parameters and use 
     linear interpolation to itteratively find best h to achieve |ARL-target_ARL|<tol'''
-    
+    start_time_op = time()
     #function to minimise
-    f = lambda L : (target_ARL - spm_iterate(L=L,n=n,chart_obj=chart_obj,distribution=sts.norm(loc=0,scale=1))[0])**2
+    f = lambda L : np.abs(target_ARL - spm_iterate(L=L,n=n,chart_obj=chart_obj,distribution=sts.norm(loc=0,scale=1))[0])
 
     min_func_results = minimize(fun=f,
                                 x0=[initial_h],
                                 method='Nelder-Mead',
                                 tol=tol,
+                                bounds=[bounds],
                                 options={'maxiter':max_its,
-                                        'disp':True})
+                                        'disp':True,
+                                        'return_all':True})
+    
+    run_time_op =np.round(time()-start_time_op+0.4,decimals=0)
+    print("Optimization Time: {} ".format(timedelta(seconds=run_time_op)))
 
     return min_func_results.x
 
@@ -94,6 +99,9 @@ dist = sts.norm(loc=0,scale=1)
 #first_t = spm_simulate(chart_obj=test_chart,distribution=dist,tau=1,max_it=1000)
 #print(first_t)
 
-#m = spm_iterate(n=1000,chart_obj=test_chart,distribution=dist)
-m = spm_optimize_h(n=100,chart_obj=test_chart,initial_h=2.6,tol=30,max_its=5)
+#m = spm_iterate(n=10000,chart_obj=test_chart,distribution=dist)
+m = spm_optimize_h(n=500,chart_obj=test_chart,initial_h=2.6,tol=1,max_its=5)
 print(m)
+
+#from scipy.optimize import show_options
+#show_options(solver='minimize',method="Nelder-Mead")
