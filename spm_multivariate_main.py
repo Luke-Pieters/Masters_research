ebb106c0808e63de68d7f4382772bb44750c0279
Luke-Pieters <u18015222@tuks.co.za>
2023-07-04 14:44:22 +0200

    updated multivariate sim and modules

--- spm_multivariate_main.py
+++ spm_multivariate_main.py
@@ -7,6 +7,7 @@ from os import makedirs
 import logging
 from time import time
 from datetime import timedelta
+import json
 
 logging.basicConfig(format='%(message)s')
 log = logging.getLogger(__name__)
@@ -19,51 +20,42 @@ log = logging.getLogger(__name__)
 #==========================================================================
 
 #ITERATE PARAMETERS
-n=50
+n=20000
 tau = 1 
-p=10
 
 #CHART TO TEST
-chart = spm_schemes.MEWMA(p=p)
+chart = spm_schemes.EHWMA()
 chart_name = chart.__class__.__name__
+use_k = False
+p = 2
 
+print("MULTIVARIATE CHART SIMULATIONS")
 print("Chart: " + chart_name)
-print("Dimentions: {}".format(p))
+print(f"Dimentions: {p}")
 
 #SHIFT SIZES 
-# delta = [0,0.25]
 delta = np.arange(0,3.25,0.25)
 
 print("Shift Sizes:")
 print(delta)
 
 #CHART PARAMETERS 
-phi_arr = [0.1,0.25,0.5]
+phi_arr = [0.1,0.25]
 
-# phi2_arr = [0.01,0.02,0.05,0.09,
-#             0.01,0.02,0.1,0.15,
-#              0.1,0.2,0.3,0.4]
+phi2_arr = {0.1: [0.01,0.05,0.09],
+            0.25: [0.05,0.1,0.2],
+            0.5: [0.1,0.2,0.4],
+            0.9: [0.2,0.5,0.8]} 
 
-k_arr = [1,2,3,4,
-        1,2,3,4,
-        1,2,3,4]
+# k_arr = [1,2,3,4,
+#         1,2,3,4,
+#         1,2,3,4]
 
-L_arr = [2.516,
-2.54,
-2.6,
-2.772,
-2.763,
-2.762,
-2.804,
-2.803,
-2.794,
-2.804,
-2.809,
-2.801,
-] #should be len= sum of (len of parameters)
 
-#DISTRIBUTION TO SIMULATE FROM
-# dist = sts.norm(loc=0,scale=1)
+opt_k = lambda x: -x/2
+
+with open(f'results\multivar_{chart_name}_optimal_L.json') as json_file:
+       L_arr = json.load(json_file)
 
 
 if 'phi2_arr' in globals():
@@ -74,12 +66,15 @@ if 'phi2_arr' in globals():
         exit()
 
     #parameters 
-    sim_parameters = np.repeat(phi_arr,4)
-    sim_parameters = np.stack([sim_parameters,phi2_arr],axis=-1)
+    sim_parameters = []
+    for phi in phi_arr:
+        for phi2 in phi2_arr[phi]:
+            sim_parameters += [[phi,phi2]]
 
     #ouput df
-    output_df = pd.DataFrame(columns=['ARL','SDRL','MRL','L','Phi','Phi2','Delta'])
-elif 'k_arr' in globals():
+    output_df = pd.DataFrame(columns=['ARL','SDRL','MRL','L','Phi','Phi2','Delta','Parameter_string'])
+
+elif use_k:
     #check parameters match chart
     accepted_charts = ["MEWMA","MHWMA"]
     if chart_name not in accepted_charts:
@@ -87,11 +82,12 @@ elif 'k_arr' in globals():
         exit()
 
     #parameters
-    sim_parameters = np.repeat(phi_arr,4)
-    sim_parameters = np.stack([sim_parameters,k_arr],axis=-1)
+    sim_parameters = []
+    for phi in phi_arr:
+        sim_parameters += [[phi,opt_k(phi)]]
 
     #ouput df
-    output_df = pd.DataFrame(columns=['ARL','SDRL','MRL','L','Phi','k','Delta'])    
+    output_df = pd.DataFrame(columns=['ARL','SDRL','MRL','L','Phi','k','Delta','Parameter_string'])    
 else:
     #check parameters match chart
     accepted_charts = ["EWMA","HWMA"]
@@ -103,7 +99,7 @@ else:
     sim_parameters = phi_arr
 
     #ouput df
-    output_df = pd.DataFrame(columns=['ARL','SDRL','MRL','L','Phi','Delta'])
+    output_df = pd.DataFrame(columns=['ARL','SDRL','MRL','L','Phi','Delta','Parameter_string'])
 
 
 print("Simulation Parameters:")
@@ -112,13 +108,13 @@ print(sim_parameters)
 total_parameters = len(sim_parameters)
 
 #Check L_arr Length
-if len(L_arr) != total_parameters:
-        log.warning('(!) Error: L array not correct length')
-        exit()
+# if len(L_arr.values) != total_parameters:
+#         log.warning('(!) Error: L array not correct length')
+#         exit()
 
 #setup and set folders to save results
 filepath = "./results/multivariate_results"
-filename = filepath + "/" + chart_name + "_p{}_ARL_SDRL_MRL_results.csv".format(p)
+filename = filepath + f"/p{p}_{chart_name}_ARL_SDRL_MRL_results.csv"
 makedirs(filepath, exist_ok=True) 
 
 #==========================================================================
@@ -127,57 +123,71 @@ makedirs(filepath, exist_ok=True)
 start_time = time()
 
 for d in delta:
-    # DISTRIBUTION TO SAMPLE FROM 
-    mu1 = np.sqrt((d**2)/p)
-
-    dist = dist = sts.norm(loc=mu1,scale=1)
+    #DISTRIBUTION TO SAMPLE FROM 
+    ds = np.ones(p)@np.array([np.sqrt((d**2)/p)]*p)
+    dist = sts.norm(loc=ds,scale=1)
     print('========================')
     print("Delta: {:.2f}".format(d))
     print('========================')
     for i in range(total_parameters):
         parms_in_use = sim_parameters[i]
         
+        #select parameters
         if 'phi2_arr' in globals():
-            chart.change_parameters(phi=parms_in_use[0],phi2=parms_in_use[1])
-            print("Parameters:","Phi: {:.2f}".format(parms_in_use[0]),"Phi2: {:.2f}".format(parms_in_use[1]),"L: {:.2f}".format(L_arr[i]))
-        elif 'k_arr' in globals():
-            chart.change_parameters(phi=parms_in_use[0],k=parms_in_use[1])
-            print("Parameters:","Phi: {:.2f}".format(parms_in_use[0]),"k: {:.2f}".format(parms_in_use[1]),"L: {:.2f}".format(L_arr[i]))
+            phi = parms_in_use[0]
+            phi2 = parms_in_use[1]
+            L = L_arr[f"p={p}"][str(phi)][str(phi2)]
+            chart.change_parameters(phi=phi,phi2=phi2)
+            print(f"Parameters: Phi: {phi}, Phi2: {phi2}, L: {L}")
+            parm_str = f'phi={phi},phi2={phi2}'
+        elif use_k:
+            phi = parms_in_use[0]
+            k = parms_in_use[1]
+            L = L_arr[f"p={p}"][str(phi)]
+            chart.change_parameters(phi=phi,k=k)
+            print(f"Parameters: Phi: {phi}, K: {k}, L: {L}")
+            parm_str = f'phi={phi},k={k}'
         else:
-            chart.change_parameters(phi=parms_in_use)  
-            print("Parameters:","Phi: {:.2f}".format(parms_in_use),"L: {:.2f}".format(L_arr[i])) 
+            phi = parms_in_use
+            L = L_arr[f"p={p}"][phi]
+            chart.change_parameters(phi=phi)  
+            print(f"Parameters: Phi: {phi}, L: {L}") 
+            parm_str = f'phi={phi}'
 
+        results = spm_sim.spm_iterate(chart_obj=chart,distribution=dist,n=n,tau=tau,standardise=False,L=L,p=p) #ARL,SDRL,MRL 
 
-        results = spm_sim.spm_iterate(p=p,chart_obj=chart,distribution=dist,n=n,tau=tau,standardise=False) #ARL,SDRL,MRL 
+        print(results)
 
-        
         if 'phi2_arr' in globals():
-            newrow = pd.Series({'ARL':results[0],
-                                'SDRL':results[1],
-                                'MRL':results[2],
-                                'L':L_arr[i],
-                                'Phi':parms_in_use[0],
-                                'Phi2':parms_in_use[1],
-                                'Delta': d})
-            output_df = pd.concat([output_df,newrow.to_frame().T],ignore_index=True)
-        elif 'k_arr' in globals():
-            newrow = pd.Series({'ARL':results[0],
-                                'SDRL':results[1],
-                                'MRL':results[2],
-                                'L':L_arr[i],
-                                'Phi':parms_in_use[0],
-                                'k':parms_in_use[1],
-                                'Delta': d})
-            output_df = pd.concat([output_df,newrow.to_frame().T],ignore_index=True)
+            newrow = pd.Series({'ARL':results['ARL'],
+                                'SDRL':results['SDRL'],
+                                'MRL':results['MRL'],
+                                'L':L,
+                                'Phi':phi,
+                                'Phi2':phi2,
+                                'Delta': d,
+                                'Parameter_string': parm_str})
+            
+        elif use_k in globals():
+            newrow = pd.Series({'ARL':results['ARL'],
+                                'SDRL':results['SDRL'],
+                                'MRL':results['MRL'],
+                                'L':L,
+                                'Phi':phi,
+                                'k':k,
+                                'Delta': d,
+                                'Parameter_string': parm_str})
+            
         else:
-            newrow = pd.Series({'ARL':results[0],
-                                'SDRL':results[1],
-                                'MRL':results[2],
-                                'L':L_arr[i],
-                                'Phi':parms_in_use,
-                                'Delta': d})
-            output_df = pd.concat([output_df,newrow.to_frame().T],ignore_index=True)
-
+            newrow = pd.Series({'ARL':results['ARL'],
+                                'SDRL':results['SDRL'],
+                                'MRL':results['MRL'],
+                                'L':L,
+                                'Phi':phi,
+                                'Delta': d,
+                                'Parameter_string': parm_str})
+    
+        output_df = pd.concat([output_df,newrow.to_frame().T],ignore_index=True)
         output_df.to_csv(filename,index=False,mode='w') #update csv file 
 
 #==========================================================================
