8a79dced7e260eed3962a249e55cfbabf112a2c1
Luke-Pieters <u18015222@tuks.co.za>
2023-07-06 20:44:35 +0200

    multivar for extended and modified done

--- multivariate_optimze.py
+++ multivariate_optimze.py
@@ -16,7 +16,7 @@ logging.basicConfig(filename=f"logs/Log-{current_time}",
 
 #CHART TO TEST
 p=4
-chart = spm_schemes.EHWMA(p=p)
+chart = spm_schemes.MHWMA(p=p)
 chart_name = chart.__class__.__name__
 
 print("Chart: " + chart_name)
@@ -38,14 +38,14 @@ phi_parm = []
 L_value = []
 
 for phi in phi_arr:
-    for phi2 in phi2_arr[phi]:
+    #for phi2 in phi2_arr[phi]:
         k = opt_k(phi)
-        logging.info(f"Running Optimize for Multivariate p={p} {chart_name}({phi},{phi2})")
-        chart.change_parameters(phi=phi,phi2=phi2)
+        logging.info(f"Running Optimize for Multivariate p={p} {chart_name}({phi},{k})")
+        chart.change_parameters(phi=phi,k=k)
         res = sim.spm_optimize_L(chart_obj=chart,initial_L=initial_L,n=10000,tol=10,p=p)
             
-        logging.info(f'Optimal L for {chart_name}({phi},{phi2}):  {res}')
-        print(f'Optimal L for {chart_name}({phi},{phi2}):  {res}')
+        logging.info(f'Optimal L for {chart_name}({phi},{k}):  {res}')
+        print(f'Optimal L for {chart_name}({phi},{k}):  {res}')
 
 logging.info(f'Optimization Complete for Multivariate p={p} {chart_name}')
 print('Optimization Complete')
