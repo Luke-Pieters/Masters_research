44f5f76416ee4ce4e59deb0c68a015b34e2a4bbf
Luke-Pieters <u18015222@tuks.co.za>
2023-07-12 07:06:33 +0200

    opt done for mhwma multi_ewma

--- multivariate_optimze.py
+++ multivariate_optimze.py
@@ -2,6 +2,8 @@ import spm_multivariate_sim_module as sim
 import Multivariate_Schemes_module as spm_schemes
 import logging
 import numpy as np
+import json
+from numpy import round
 
 from datetime import datetime
 now = datetime.now()
@@ -17,15 +19,13 @@ logging.basicConfig(filename=f"logs/Log-{current_time}",
 
 #CHART TO TEST
 p=4
-chart = spm_schemes.EEWMA(p=p)
+chart = spm_schemes.EWMA(p=p)
 chart_name = chart.__class__.__name__
 
-print("Chart: " + chart_name)
-
-initial_L = 12.8
+print("Chart: " + chart_name + f"p={p}")
 
 #CHART PARAMETERS 
-phi_arr = [0.1]
+phi_arr = [0.1,0.25]
 
 phi2_arr = {0.1: [0.01,0.05,0.09],
             0.25: [0.05,0.1,0.2],
@@ -37,22 +37,25 @@ opt_k = lambda x: -x/2
 phi_parm = []
 # k_parm = []
 
+with open(f'results\multivar_{chart_name}_optimal_L.json') as json_file:
+    L_arr = json.load(json_file)
 
 for phi in phi_arr:
-    for phi2 in phi2_arr[phi]:
-        L_value = []
-        for j in range(3):
-            k = opt_k(phi)
-            logging.info(f"Running Optimize for Multivariate p={p} {chart_name}({phi},{phi2})")
-            chart.change_parameters(phi=phi,phi2=phi2)
-            res = sim.spm_optimize_L(chart_obj=chart,initial_L=initial_L,n=10000,tol=10,p=p)
-            L_value += [res[0]]
-            print(L_value)
+    # for phi2 in phi2_arr[phi]:
+        k = opt_k(phi)
+        logging.info(f"Running Optimize for {chart_name}({phi})")
+        chart.change_parameters(phi=phi,k=k)
+        initial_L = L_arr[f"p={p}"][str(phi)]
+        # initial_L = L_arr[f"p={p}"][str(phi)][str(phi2)]
+        res = sim.spm_optimize_L(chart_obj=chart,initial_L=initial_L,n=50000,tol=1,p=p)
             
-        opt_L = np.mean(L_value)
-        logging.info(f'Optimal L for Multivariate p={p} {chart_name}({phi},{phi2}):  {res}')
-        print(f'Optimal L for Multivariate p={p} {chart_name}({phi},{phi2}):  {res}')
+        logging.info(f'Optimal L for {chart_name}({phi}):  {res}')
+        print(f'Optimal L for p{p} {chart_name}({phi}):  {res}')
+        L_arr[f"p={p}"][str(phi)] = round(res[0],5)
+        # L_arr[f"p={p}"][str(phi)][str(phi2)] = round(res[0],5)
 
+        with open(f'results\multivar_{chart_name}_optimal_L.json', 'w') as json_file:
+            json.dump(L_arr, json_file)
 
-logging.info(f'Optimization Complete for Multivariate p={p} {chart_name}')
+logging.info(f'Optimization Complete for {chart_name} p{p}')
 print('Optimization Complete')
