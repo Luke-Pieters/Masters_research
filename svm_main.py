08c1a7fcd7542a608e763eb341ff19ee2551691a
Luke-Pieters <u18015222@tuks.co.za>
2023-09-04 21:36:30 +0200

    added RF and DT comparisons

--- svm_main.py
+++ svm_main.py
@@ -67,16 +67,16 @@ X_train, X_test, id_y_train, id_y_test = train_test_split(df[['B0','B1','B2','S2
 
 x_names = X_train.columns
 
-# print('Training ID Model: DT')
-# parm_mdl = DecisionTreeClassifier()
-# # parm_mdl = svm.LinearSVC(multi_class='ovr',max_iter=10000,C=0.8)
-# parm_mdl.fit(X_train, id_y_train)
-# parm_pred = parm_mdl.predict(X_test)
+print('Training ID Model: DT')
+parm_mdl = DecisionTreeClassifier()
+# parm_mdl = svm.LinearSVC(multi_class='ovr',max_iter=10000,C=0.8)
+parm_mdl.fit(X_train, id_y_train)
+parm_pred = parm_mdl.predict(X_test)
 
-# print("="*30)
-# print('ID Model Metrics')
-# print("Accuracy:",metrics.accuracy_score(id_y_test, parm_pred))
-# print("="*30)
+print("="*30)
+print('ID Model Metrics')
+print("Accuracy:",metrics.accuracy_score(id_y_test, parm_pred))
+print("="*30)
 
 print('Training ID Model: SVM')
 # parm_mdl = DecisionTreeClassifier()
@@ -92,11 +92,35 @@ print("="*30)
 #SHIFT SIZE IDENTIFIEER
 X_train, X_test, ss_y_train, ss_y_test = train_test_split(df[['B0','B1','B2','S2','T2']], df['Shift_size'], test_size=0.3,random_state=109) # 70% training and 30% test
 
+print('Training Shift-Size Model: SVM')
+ss_mdl = svm.LinearSVC(multi_class='ovr',max_iter=10000,C=0.8)
+ss_mdl.fit(X_train, ss_y_train)
+ss_pred = ss_mdl.predict(X_test)
+
+print("="*30)
+print('Shift-Size Model Metrics')
+print("Accuracy:",metrics.accuracy_score(ss_y_test, ss_pred))
+print("="*30)
+
 print('Training Shift-Size Model: DT')
+ss_mdl = DecisionTreeClassifier()
+ss_mdl.fit(X_train, ss_y_train)
+ss_pred = ss_mdl.predict(X_test)
+
+print("="*30)
+print('Shift-Size Model Metrics')
+print("Accuracy:",metrics.accuracy_score(ss_y_test, ss_pred))
+print("="*30)
+
+print('Training Shift-Size Model: RF')
 ss_mdl = RandomForestClassifier()
 ss_mdl.fit(X_train, ss_y_train)
 ss_pred = ss_mdl.predict(X_test)
 
+print("="*30)
+print('Shift-Size Model Metrics')
+print("Accuracy:",metrics.accuracy_score(ss_y_test, ss_pred))
+print("="*30)
 # viz_model = dtreeviz.model(ss_mdl,
 #                            X_train=X_train, y_train=ss_y_train,
 #                            feature_names=x_names,
@@ -107,20 +131,7 @@ ss_pred = ss_mdl.predict(X_test)
 # v.show()   
              
 
-print("="*30)
-print('Shift-Size Model Metrics')
-print("Accuracy:",metrics.accuracy_score(ss_y_test, ss_pred))
-print("="*30)
 
-# print('Training Shift-Size Model: SVM')
-# ss_mdl = svm.LinearSVC(multi_class='ovr',max_iter=10000,C=0.8)
-# ss_mdl.fit(X_train, ss_y_train)
-# ss_pred = ss_mdl.predict(X_test)
-
-# print("="*30)
-# print('Shift-Size Model Metrics')
-# print("Accuracy:",metrics.accuracy_score(ss_y_test, ss_pred))
-# print("="*30)
 
 #==================================================================================
 #==================================================================================
